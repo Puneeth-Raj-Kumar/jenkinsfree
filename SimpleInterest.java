bf7de053073500764220fc09257b49f702628e3e
Puneeth Raj Kumar M S <puneethms0707@gmail.com>
2026-01-30 09:52:40 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -1,6 +1,6 @@
 class SimpleInterest{
 public static void main(String[] args){
 int p=7,t=6,r=5;
-System.out.println("Simple interest" +(p*t*r)/100);
+System.out.println("Simple interest" ,+(p*t*r)/100);
 }
 }
